407e79f3ccd2d729aed6e57f4132f6c9b1e024d0
alyson amtman <aamtman@gmail.com>
2021-03-25 21:20:42 -0600

    UFO 2 file

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -1,32 +1,14 @@
 var tableData = data;
-var tableBody = d3.select("tbody");
-function buildData(data){
-    tableBody.html("");
-    data.forEach((tableRow)=>{
-        var Row= tableBody.append("tr");
-        Object.values(tableRow).forEach((Value)=>{
-            var tableCell=Row.append("td");
-            tableCell.text(Value);
-        })
-    })
-}
+
 // YOUR CODE HERE!
-buildData(tableData)
 
-function filterData(){
-    var tableDate = d3.select ("#datetime").property("value");
-    var filterData = tableData;
-    filterData= filterData.filter(datarow=>datarow.datetime===tableDate);
-    buildData(filterData);
-}
-
-d3.selectAll("#filter-btn").on("click",filterData);
-
+// Creating References to tbody, input and button
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
-var columns = ["datetime", "city", "state", "country", "shape", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
 
 // Inputing the data into the HTML
 var addData = (dataInput) => {
@@ -40,34 +22,31 @@ var addData = (dataInput) => {
 addData(tableData);
 
 
-// Creating an Event Listener for the Button
-// Setting up the Filter Button for Date and City
+//Create eventListener for button
 button.on("click", () => {
 
-    d3.event.preventDefault();
-    
-
-    var inputDate = inputFieldDate.property("value").trim();
-    // console.log(inputDate)
-    // trim the inputs 
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-    
-
-    $tbody.html("");
-
+  d3.event.preventDefault();
+  var inputDate = inputFieldDate.property("value").trim();
+  // console.log(inputDate)
+  var inputCity = inputFieldCity.property("value").toLowerCase().trim();
+  // console.log(inputCity)
+  var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+  // console.log(filterDate)
+  var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+  // console.log(filterCity)
+  var filterCombinedData = tableData.filter(tableData => tableData.datetime === inputDate && tableData.city === inputCity);
+  // console.log(filterCombinedData)
+  $tbody.html("");
     let response = {
-        filterDate
+        filterDate, filterCity, filterCombinedData
     }
-
-
-    if(response.filterDate.length !== 0) {
-        addData(filterDate);
-    }
-
-    // add comment if not sightings
-    
-        else {
-            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
-        }
+    if(response.filterCombinedData.length !== 0) {
+      addData(filterCombinedData);
+  }
+  else if(response.filterCombinedData.length === 0 && ((response.filterDate.length !== 0 || response.filterCity.length !== 0))) {
+    addData(filterDate) || addData(filterCity);
+}
+else {
+  $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+}
 })
-
